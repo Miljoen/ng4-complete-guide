ddf28708ba07ca288bc582aee91b8d011edf87ba
Yoeri J. Moens <yoerimoens@gmail.com>
2022-07-01 16:19:37 +0200

    (55) Loop over recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model"
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    public recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1.jpg'),
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1.jpg'),
+    ]
 
   constructor() { }
 
